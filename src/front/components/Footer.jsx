44c8531c7e800e09abf6fb18310f14b4071c94f7
Ricky <rikigibson199@gmail.com>
2025-06-11 19:34:18 +0000

    proceso

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,11 +1,32 @@
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/react-flask-template">template documentation</a> <i className="fa-solid fa-file"></i> for help.
-		</p>
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+import React from "react";
+
+export const Footer = () => {
+	return (
+	<div class="container">
+	<footer className="py-3 my-4">
+		<ul class="nav justify-content-center border-bottom pb3 mb-3">
+			<li class="nav-item"><a href="x" class="nav-link px-2 text-muted">HOME</a></li>
+			<li class="nav-item"><a href="x" class="nav-link px-2 text-muted">HOME</a></li>
+			<li class="nav-item"><a href="x" class="nav-link px-2 text-muted">HOME</a></li>
+		</ul>
+		<div class="container">
+  <div class="row justify-content-end">
+    <div class="col-md-3">
+      <h5>Título 1</h5>
+      <p>Contenido de la columna 1</p>
+    </div>
+    <div class="col-md-3">
+      <h5>Título 2</h5>
+      <p>Contenido de la columna 2</p>
+    </div>
+    <div class="col-md-3">
+      <h5>Título 3</h5>
+      <p>Contenido de la columna 3</p>
+    </div>
+  </div>
+</div>
+		<p class="text-center text-muted"> Lo que sea </p>
 	</footer>
-);
+	</div>
+)
+};
